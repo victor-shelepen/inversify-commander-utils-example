f3675d953202bf982403765b2292b76a930689f7
vlikin <viktor.likin@gmail.com>
2019-01-13 03:34:28 +0200

    The example was changed according to the library,

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
-import {controller, action, build} from 'inversify-commander-utils';
+import {group, action, build, registerGroups} from 'inversify-commander-utils';
 import {Container, inject, injectable} from 'inversify';
-import * as commander from 'commander';
+import program from 'commander';
 
 @injectable()
 class TodoContainer {
@@ -10,22 +10,29 @@ class TodoContainer {
     }
 }
 
-@controller('printer')
+@group('printer')
 class TestController {
 
     @inject(TodoContainer)
     public todoContainer!: TodoContainer;
 
-    @action('A')
-    public testA() {
-        console.log(this.todoContainer.printA());
+    @action(
+        'A <parameter>',
+        [
+            { pattern: '-c, --count <mode>', description: 'Number of prints.' }
+        ]
+    )
+    public testA(parameter: string, command: any) {
+        console.log(this.todoContainer.printPaper());
+        console.log(parameter, command.count);
     }
 
 }
 
 const container = new Container();
 container.bind(TodoContainer).to(TodoContainer);
-build(commander, container);
+registerGroups(container);
+build(program, container);
 
-commander
+program
     .parse(process.argv);
